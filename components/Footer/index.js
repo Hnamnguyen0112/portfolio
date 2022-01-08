dd29c2e1aa6e811f5d2273caf4552158460d33d1
Nam Nguyen <alexnguyenth96@gmail.com>
2022-01-08 15:26:39 +0700

    split components

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="bg-black border border-white rounded-lg mt-10">
       <div className="flex flex-col md:flex-row md:justify-between items-center text-sm text-white py-10">
-        <p className="order-2 md:order-1"> &copy; Nam Nguyen, 2021. </p>
+        <p className="mx-auto order-2 md:order-1"> &copy; Nam Nguyen, 2021. </p>
       </div>
     </div>
   )
