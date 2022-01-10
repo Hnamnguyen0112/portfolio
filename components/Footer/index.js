c4d16bca5dca052ed8042a31f85f647c66729707
Nam Nguyen <alexnguyenth96@gmail.com>
2022-01-10 22:57:16 +0700

    add image footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,13 +1,17 @@
-import { AppContext } from '../../context/state'
 import { useContext } from 'react'
+import Image from 'next/image'
+import { AppContext } from '../../context/state'
 
 export default function Footer() {
   const { mode } = useContext(AppContext)
 
   return (
-    <div className={'max-w-5xl mx-auto border-4 rounded-lg mt-10 ' + (mode ? 'bg-black border-white' : 'border-amber-300')}>
+    <div className={'max-w-5xl mx-auto rounded-lg mt-10 relative'}>
       <div className={'flex flex-col md:flex-row md:justify-between items-center text-sm py-10 ' + (mode ? 'text-white' : '')}>
         <p className="mx-auto order-2 md:order-1"> &copy; Nam Nguyen, 2021. </p>
+      </div>
+      <div className="absolute bottom-0 right-0">
+        <Image src={'/bongo-trim.png'} width={147} height={70} />
       </div>
     </div>
   )
