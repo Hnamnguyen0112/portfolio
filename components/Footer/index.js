543e7be5ed396db5b75119d95421541785d80734
Nam Nguyen <alexnguyenth96@gmail.com>
2022-01-10 23:01:10 +0700

    add image footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <div className={'max-w-5xl mx-auto rounded-lg mt-10 relative'}>
       <div className={'flex flex-col md:flex-row md:justify-between items-center text-sm py-10 ' + (mode ? 'text-white' : '')}>
-        <p className="mx-auto order-2 md:order-1"> &copy; Nam Nguyen, 2021. </p>
+        <p className="mx-auto order-2 md:order-1"> &copy; Nam Nguyen.</p>
       </div>
       <div className="absolute bottom-0 right-0">
         <Image src={'/bongo-trim.png'} width={147} height={70} />
