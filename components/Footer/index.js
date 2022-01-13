10125708aba0dd3a303b554be3cfd8bb9bc99d56
Nam Nguyen <alexnguyenth96@gmail.com>
2022-01-13 23:52:50 +0700

    refactor code

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <div className={'max-w-5xl mx-auto rounded-lg relative'}>
-      <div className='flex flex-col md:flex-row md:justify-between items-center text-sm py-10 text-white'>
+    <footer className={'max-w-5xl mx-auto rounded-lg bottom-0'}>
+      <div className='relative flex flex-col md:flex-row md:justify-between items-center text-sm py-10 text-white'>
         <p className="mx-auto order-2 md:order-1"> &copy; Nam Nguyen.</p>
+        <div className="absolute bottom-0 right-0">
+          <Image src={'/bongo-trim.png'} width={147} height={70} />
+        </div>
       </div>
-      <div className="absolute bottom-0 right-0">
-        <Image src={'/bongo-trim.png'} width={147} height={70} />
-      </div>
-    </div>
+    </footer>
   )
 }
