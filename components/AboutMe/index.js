7d5da52d01dba80b28284e9b3cce7c549ae61dbf
Nam Nguyen <alexnguyenth96@gmail.com>
2022-01-17 00:56:11 +0700

    contact form

--- components/AboutMe/index.js
+++ components/AboutMe/index.js
@@ -3,9 +3,11 @@ export default function AboutMe() {
     <div id="about-me"
          className='mt-20 mx-auto max-w-xl min-w-0 p-4 rounded-lg shadow-lg bg-[#022E53]'>
       <p className='text-xl mb-4 font-semibold text-[#55FCD3]'>#About me</p>
-      <p>
+      <p className='text-md font-semibold'>
         I&apos;m building diverse & highly accessible applications for everyone, also working as a Software Engineer
         in Vietnam.
+        <br/> <br/>
+        So far I&apos;ve always tried to create memorable products that are not only enjoyable to use but are written in code that&apos;s maintainable and easy to understand.
       </p>
     </div>
   )
