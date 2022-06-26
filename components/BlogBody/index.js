de9a438275090779a82442fd8c12a9d4f4a32a07
Nam Nguyen <alexnguyenth96@gmail.com>
2022-06-27 02:10:04 +0700

    update portfolio

--- components/BlogBody/index.js
+++ components/BlogBody/index.js
@@ -2,7 +2,7 @@ import { MDXRemote } from 'next-mdx-remote';
 
 export default function BlogBody({ content }) {
   return (
-    <div className="prose lg:prose-xl max-w-2xl mx-auto">
+    <div className="prose md:prose-lg lg:prose-xl max-w-2xl mx-auto prose-img:rounded-xl">
       <MDXRemote {...content} />
       <style jsx>{`
         .prose {
