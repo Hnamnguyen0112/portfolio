df59edd7ffdf9a322711a326111d2b0226ed8693
Nam Nguyen <alexnguyenth96@gmail.com>
2022-06-27 23:43:24 +0700

    update portfolio

--- pages/[slug].js
+++ pages/[slug].js
@@ -45,7 +45,7 @@ export default function BlogDetail({ data, content, slug, readingTime }) {
                 <title>{data.title}</title>
                 <meta property="og:title" content={data.title} />
                 <meta property="og:description" content={data.excerpt} />
-                <meta property="og:image" content={data.url} />
+                <meta property="og:image" content={`https://namnguyen.io/blogs/${data.url}`} />
               </Head>
               <BlogHeader
                 title={data.title}
