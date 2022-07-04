3fceb14276265f9607354945bcc99a895ff315b3
Nam Nguyen <alexnguyenth96@gmail.com>
2022-07-05 00:14:25 +0700

    blog update

--- components/BlogHeader/index.js
+++ components/BlogHeader/index.js
@@ -8,13 +8,7 @@ export default function BlogHeader({ title, coverImage, date, readingTime }) {
         {title}
       </h1>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage
-          title={title}
-          src={coverImage}
-          height={620}
-          width={1240}
-          placeholder="blur"
-        />
+        <CoverImage title={title} src={coverImage} height={620} width={1240} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="mb-6 text-lg">
