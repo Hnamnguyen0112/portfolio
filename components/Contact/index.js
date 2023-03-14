0a37f0b3075f735888b7c634a077f1b837894579
Nam Nguyen <alexnguyenth96@gmail.com>
2023-03-14 23:15:27 +0700

    update cv

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -140,7 +140,7 @@ export default function Contact() {
             Send Message
           </button>
           <NextLink
-            href="/Nguyen-Thanh-Hai-Nam.pdf"
+            href="/alex-nguyen.pdf"
             className="sm:mt-0 uppercase text-sm font-bold tracking-wide bg-cyan-600 p-3 rounded-lg focus:outline-none focus:shadow-outline text-white"
             target="_blank"
             rel="noreferrer"
